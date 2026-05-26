202b1290d5f4168a08e2f894fe8eced5a0655daa
jersonpaulcelestino-lang <jersonpaulcelestino@gmail.com>
2026-05-25 21:56:09 -0500

    Permitir agregar nuevos registros

--- fronted/app.js
+++ fronted/app.js
@@ -21,3 +21,34 @@ async function loadData() {
 
     document.getElementById("result").innerHTML = rows;
 }
+
+async function createTransaction() {
+
+    const status = document.getElementById("status").value;
+
+    const amount = parseInt(
+        document.getElementById("amount").value
+    );
+
+    const response = await fetch(
+        "https://jerson-dashboard-api.azurewebsites.net/api/CreateTransaction",
+        {
+            method: "POST",
+
+            headers: {
+                "Content-Type": "application/json"
+            },
+
+            body: JSON.stringify({
+                status: status,
+                amount: amount
+            })
+        }
+    );
+
+    const data = await response.json();
+
+    alert(data.message);
+
+    loadData();
+}
